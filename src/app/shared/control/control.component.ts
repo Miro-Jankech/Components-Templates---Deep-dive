827f16b77fea80390b8dbbfed8526b46138472af
Miro-Jankech <jankechmiro@gmail.com>
2025-07-14 10:45:39 +0200

    contentChild implemented

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, HostListener, Input, ViewEncapsulation } from '@angular/core';
+import { Component, ContentChild, ElementRef, HostBinding, HostListener, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -13,9 +13,15 @@ import { Component, HostBinding, HostListener, Input, ViewEncapsulation } from '
 })
 export class ControlComponent {
   // @HostBinding('class') className = 'control';
-  @HostListener('click') onClick(){
-    console.log('Clicked!');
-    
-  }
+  //@HostListener('click') onClick(){
+    //console.log('Clicked!');  
+  //}
+
+  @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>
+
   @Input({ required: true }) label!:string;
+
+  onClick() {
+    console.log(this.control);
+  }
 }
